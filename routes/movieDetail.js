2305555d82e4b7b3e73e431ef5c41d771a6ab98d
Quest <stefan.quest@gmail.com>
2019-10-12 09:11:35 +0200

    attempting to consol.log results from aggregated details in moviedetails

--- routes/movieDetail.js
+++ routes/movieDetail.js
@@ -5,36 +5,27 @@ const router = express.Router();
 const youtube = require("../apis/youtube");
 const tmdbDetail = require("../apis/tmdbDetail");
 
-
-
-
-
 //this requires official title from tmdb call
-  // this requires movie id from tmdb
-const AggregatedDetails = async () => {
-  await tmdbDetail(550);
-  await youtube("Fight Club");
+// this requires movie id from tmdb
+const AggregatedDetails = () => {
+  const tmdb = tmdbDetail(550);
+  const yout = youtube("Fight Club");
+  console.log(tmdb, yout);
 
-  return console.log({ ...youtube("Fight Club"), ...tmdbDetail(550) });
+  // return  console.log({ ...youtube("Fight Club"), ...tmdbDetail(550) });
 };
 
 /* GET movie details */
 router.get("/", function(req, res, next) {
   AggregatedDetails();
 
-  
-
   // const checkDb =()=>{if (!title){
   //   new Movie = (...movieDetails,...movieTrailer)
   //  }}
 
-  res.send("this is the actually trailer "); 
+  res.send("this is the actually trailer ");
 });
 // let [youtubedb, tmdbDetaildb]= await Promise.all([youtube("Fight Club"), tmdbDetail(550)]);
 module.exports = router;
 
-
-
-   // display movie details and video player with trailer
-
-
+// display movie details and video player with trailer
